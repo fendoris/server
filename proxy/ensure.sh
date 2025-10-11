7c666ab311492f90acb9517aa383eea796eb4986
KC1508 <107610683+kchaney1508@users.noreply.github.com>
2025-10-11 14:58:02 -0500

    add proxy directory

---
+++ proxy/ensure.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+java -Xms1024M -Xmx1024M -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar server.jar
---
+++ proxy/start.bat
@@ -0,0 +1,5 @@
+@echo off
+
+java -Xms1024M -Xmx1024M -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar server.jar
+
+pause
